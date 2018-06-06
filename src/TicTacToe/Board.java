63fb9a562c55f1dff1a8e523c47c18e650100f04
Mifou <michalskupienn@gmail.com>
2018-06-06 23:39:53 +0200

    created zero char as static final field

--- src/TicTacToe/Board.java
+++ src/TicTacToe/Board.java
@@ -3,8 +3,8 @@ package TicTacToe;
 
 public class Board {
     private char board[][] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
-
-
+    private static final char ZERO_CHAR = (char) 48;
+    
     public void displayBoard() {
         for (int i = 0; i < board.length; i++) {
             System.out.println();
@@ -19,7 +19,7 @@ public class Board {
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == (char) (48 + position)) {
+                if (board[i][j] == ZERO_CHAR + position) {
                     board[i][j] = sign;
                     return true;
                 }
